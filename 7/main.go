a6722c81c9baaa6321982b26112611a29094bce6
Akilan S <Akilan_S@ad.infosys.com>
2018-02-02 17:34:10 +0530

    test cases added

--- 7/main.go
+++ 7/main.go
@@ -2,13 +2,42 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println(getSkills())
+	skills := getSkills()
+	fmt.Println(skills)
+	writeSkillFile("skills.txt", skills)
 }
 
 func getSkills() []string {
 	skills := []string{"PHP", "PYTHON", "RUBY"}
 	return skills
+}
+
+func writeSkillFile(filename string, skills []string) {
+	fmt.Println("Writting in a file")
+	skillsString := strings.Join(skills, ",")
+	error := ioutil.WriteFile(filename, []byte(skillsString), 0666)
+
+	if error != nil {
+		fmt.Println("Error - ", error)
+		os.Exit(1)
+	}
+}
+
+func readSkillFile(filename string) []string {
+
+	byteSlice, error := ioutil.ReadFile(filename)
+
+	if error != nil {
+		fmt.Println("Error - ", error)
+		os.Exit(1)
+	}
+
+	return []string(strings.Split(string(byteSlice), ","))
+
 }
--- 7/main_test.go
+++ 7/main_test.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
-func TestSkill(t *testing.T) {
+// Tesing number of skills
+
+func TestGetSkills(t *testing.T) {
 
 	skills := getSkills()
 
-	if len(skills) != 4 {
-		t.Errorf("Expected number of skills 4 but you have %v", len(skills))
+	if len(skills) != 3 {
+		t.Errorf("Expected number of skills 3 but you have %v", len(skills))
 	}
+
+}
+
+//// Tesing number of skills after writting
+
+func TestWriteSkillFile(t *testing.T) {
+
+	filename := "_skilltesting.txt"
+
+	skills := getSkills()
+
+	os.Remove(filename)
+
+	writeSkillFile(filename, skills)
+
+	skills = readSkillFile(filename)
+
+	if len(skills) <= 2 {
+		t.Errorf("Expected length is %v but got %v", 2, len(skills))
+	}
+
+	os.Remove(filename)
 
 }
